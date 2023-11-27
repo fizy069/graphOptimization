73abd982ed66623564031b0862dddabccafe8755
fizy069 <mahirg4@gmail.com>
2023-11-27 18:44:23 +0530

    input

--- Driver.cpp
+++ Driver.cpp
@@ -3,6 +3,9 @@
 #include "professor.h"
 #include "algorithmsSet.h"
 #include <vector>
+#include <fstream>
+#include <sstream>
+#include<chrono>
 
 using namespace std;
 
@@ -81,6 +84,8 @@ void assigningHDELECTcourses(Course courses[], int n)
 
 int main()
 {
+    auto start = std::chrono::high_resolution_clock::now();
+    freopen("output2.txt", "w", stdout);
 
     Course UGCDCcourses[] = {Course("UGCDC1", 1), Course("UGCDC2", 1), Course("UGCDC3", 1), Course("UGCDC4", 1)};
     Course UGELECTcourses[] = {Course("UGELECT1", 2), Course("UGELECT2", 2), Course("UGELECT3", 2), Course("UGELECT4", 2)};
@@ -92,6 +97,62 @@ int main()
     assigningHDCDCcourses(HDCDCcourses, sizeof(HDCDCcourses) / sizeof(HDCDCcourses[0]));
     assigningHDELECTcourses(HDELECTcourses, sizeof(HDELECTcourses) / sizeof(HDELECTcourses[0]));
 
+
+
+
+    vector<Professor> professors;
+    int numOfProf;
+    freopen("prof_input.txt", "r", stdin);
+
+    cin>>numOfProf;
+    for(int i=0; i<numOfProf; i++)
+    {
+        string s;
+        cin>>s;
+        
+        // vector<int> a;
+
+        vector<int> b;
+        int x,y,z;
+        cin>>x>>y>>z;
+        b.push_back(x);
+        b.push_back(y);
+        b.push_back(z);
+
+        
+        vector<int> c;
+        // int x,y,z;
+        cin>>x>>y>>z;
+        c.push_back(x);
+        c.push_back(y);
+        c.push_back(z);
+
+        
+        vector<int> d;
+        // int x,y,z;
+        cin>>x>>y>>z;
+        d.push_back(x);
+        d.push_back(y);
+        d.push_back(z);
+
+        
+        vector<int> e;
+        // int x,y,z;
+        cin>>x>>y>>z;
+        e.push_back(x);
+        e.push_back(y);
+        e.push_back(z);
+
+        double creds;
+        cin>>creds;
+
+        Professor p(s, b, c, d, e, creds);
+
+        professors.push_back(p);            
+        
+    }
+    
+
     // Professor professors[] = {
     //     Professor("Prof1", {3, 1, 2}, {2, 0, 1}, {2, 3, 0}, {3, 1, 2}, 1.5),
     //     Professor("Prof2", {0, 1, 2}, {2, 3, 0}, {3, 1, 2}, {1, 2, 3}, 0.5),
@@ -102,16 +163,17 @@ int main()
     //     Professor("Prof7", {1, 3, 0}, {3, 1, 2}, {3, 2, 1}, {2, 0, 1}, 1.5),
     //     Professor("Prof8", {0, 1, 3}, {2, 3, 0}, {2, 3, 0}, {1, 2, 3}, 1),
     //     Professor("Prof9", {1, 2, 3}, {1, 2, 3}, {3, 2, 0}, {3, 0, 1}, 1)};
-    vector<Professor> professors{
-        Professor("Prof1", {3, 1, 2}, {2, 0, 1}, {2, 3, 0}, {3, 1, 2}, 1.5),
-        Professor("Prof2", {0, 1, 2}, {2, 3, 0}, {3, 1, 2}, {1, 2, 3}, 0.5),
-        Professor("Prof3", {2, 3, 1}, {3, 1, 2}, {3, 2, 1}, {2, 0, 1}, 1),
-        Professor("Prof4", {1, 3, 2}, {1, 3, 0}, {3, 1, 2}, {1, 2, 3}, 1.5),
-        Professor("Prof5", {2, 3, 0}, {3, 1, 2}, {2, 3, 0}, {2, 0, 1}, 0.5),
-        Professor("Prof6", {0, 1, 2}, {2, 3, 1}, {3, 1, 2}, {1, 2, 3}, 1.5),
-        Professor("Prof7", {1, 3, 0}, {3, 1, 2}, {3, 2, 1}, {2, 0, 1}, 1.5),
-        Professor("Prof8", {0, 1, 3}, {2, 3, 0}, {2, 3, 0}, {1, 2, 3}, 1),
-        Professor("Prof9", {1, 2, 3}, {1, 2, 3}, {3, 2, 0}, {3, 0, 1}, 1)};
+
+    // vector<Professor> professors{
+    //     Professor("Prof1", {3, 1, 2}, {2, 0, 1}, {2, 3, 0}, {3, 1, 2}, 1.5),
+    //     Professor("Prof2", {0, 1, 2}, {2, 3, 0}, {3, 1, 2}, {1, 2, 3}, 0.5),
+    //     Professor("Prof3", {2, 3, 1}, {3, 1, 2}, {3, 2, 1}, {2, 0, 1}, 1),
+    //     Professor("Prof4", {1, 3, 2}, {1, 3, 0}, {3, 1, 2}, {1, 2, 3}, 1.5),
+    //     Professor("Prof5", {2, 3, 0}, {3, 1, 2}, {2, 3, 0}, {2, 0, 1}, 0.5),
+    //     Professor("Prof6", {0, 1, 2}, {2, 3, 1}, {3, 1, 2}, {1, 2, 3}, 1.5),
+    //     Professor("Prof7", {1, 3, 0}, {3, 1, 2}, {3, 2, 1}, {2, 0, 1}, 1.5),
+    //     Professor("Prof8", {0, 1, 3}, {2, 3, 0}, {2, 3, 0}, {1, 2, 3}, 1),
+    //     Professor("Prof9", {1, 2, 3}, {1, 2, 3}, {3, 2, 0}, {3, 0, 1}, 1)};
 
     // showProfessors(professors, sizeof(professors) / sizeof(professors[0]));
     showProfessors(professors);
@@ -127,6 +189,12 @@ int main()
                                       vector<Course>(Professor::UGELECTallCourse, Professor::UGELECTallCourse + Course::totalUGElectCourses),
                                       vector<Course>(Professor::HDCDCallCourse, Professor::HDCDCallCourse + Course::totalHDCDCCourses),
                                       vector<Course>(Professor::HDELECTallCourse, Professor::HDELECTallCourse + Course::totalHDElectCourses));
+    
+    auto stop = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+    
+    std::cout << "Time taken by function: " << duration.count() << " microseconds" << std::endl;
+    
     return 0;
 }
 
---
+++ t.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+
+void solve(){
+
+}
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+freopen("fi.txt", "r", stdin);
+freopen("fo.txt", "w", stdout);
+#else
+// online submission
+#endif
+   ios_base::sync_with_stdio(false);
+   cin.tie(NULL);
+ 
+int t;
+cin>>t;
+while(t--)
+{
+solve();
+}
+ 
+    return 0;
+}
